9c24f52a1f6bab930b21f48fc2db5aea2168dc49
queldis <queldis@gmail.com>
2016-03-13 14:32:01 +0200

    draft

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/upload', methods=['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
-        f = request.files['the_file']
+        f = request.files['user_essay']
         f.save('/var/www/uploads/uploaded_file.txt')
 
 @app.route('/')
