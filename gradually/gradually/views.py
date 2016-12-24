0c751a626b65a6215b78d775bc866013f3b69ac1
barisce <baris9celik4@gmail.com>
2016-12-24 12:30:42 +0200

    Added word index  array
    changed:
    -views.py
    -index.html

--- gradually/gradually/views.py
+++ gradually/gradually/views.py
@@ -4,6 +4,7 @@ import json
 from gradually.grade import predict
 from gradually.grade import grammar
 import sys
+import ast
 
 def index(request):
     print(sys.path)
@@ -17,7 +18,6 @@ def grade(request):
     print (predict_result)
     
     grammar_errors = grammar.grammar(essay_text)
-    print(type(grammar_errors))
     grammar_list = []
     correction_list = []
     for match in grammar_errors:
@@ -30,10 +30,12 @@ def grade(request):
     correction_list_j = json.dumps(correction_list, default=obj_dict)
     
     words = []
+    apperances = {}
     for match in grammar_errors:
         word = []
         for letter in range(match.fromx, match.tox):
             word.append(essay_text[letter])
+        apperances[match.fromx] = match.tox
         words.append(word)
         word = []
     
@@ -43,18 +45,26 @@ def grade(request):
         i = i+1
     print (words)
     wrong_words = json.dumps(words, default=obj_dict)
+    dict_of_apperances = json.dumps(apperances, default=obj_dict)
+    print ()
+    dict_of_apperances = dict_of_apperances.replace("\"", " ")
+    dict_of_apperances = dict_of_apperances.replace(" ", "")
+    print(type(dict_of_apperances))
+    print (dict_of_apperances)
+    print ()
     #testing 1 2 3
     #for match in grammar_errors:
     #    print(match)
     
     #"Nope. Not even going to bother analyzing that. Good luck. \
                      #<br> (actually I would but it does not work yet.)"
-    return JsonResponse({'essay'     : essay_text,
-                         'spell'     : "<img src=http://cultofthepartyparrot.com/parrots/parrot.gif>",
-                         'grade'     : predict_result,
-						 'grammar'   : grammar_list_j,
-						 'correction': correction_list_j,
-						 'wrong_words': wrong_words
+    return JsonResponse({'essay'     			: essay_text,
+                         'spell'     			: "<img src=http://cultofthepartyparrot.com/parrots/parrot.gif>",
+                         'grade'     			: predict_result,
+						 'grammar'   			: grammar_list_j,
+						 'correction'			: correction_list_j,
+						 'wrong_words'			: wrong_words,
+						 'dict_of_apperances'	: dict_of_apperances
                          # "aundera - that <b>cunt</b> <br> also <b>dat</b> <i>non-escaped</i> output\
                          # <br> <img src=http://cultofthepartyparrot.com/parrots/parrot.gif> "
                         }, safe=False)
